5fd4a39420f91a463938464445c6d2341b65acd5
luochao <luochao@qutoutiao.net>
2019-06-21 11:10:14 +0800

    first commit

---
+++ src/Timorchao.php
@@ -0,0 +1,15 @@
+namespace Timorchao\Test;
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lc-timorchao
+ * Date: 2019/6/21
+ * Time: 10:54
+ */
+
+class Timorchao
+{
+    public function helloWorld(){
+        echo "Hello World!";
+    }
+}
